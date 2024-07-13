551668c646f86d6a7e594f539027686afcefbadd
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-07-13 13:10:01 +0100

    Add initial controllers

--- job-platform/routes/web.php
+++ job-platform/routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\EmployerProfileController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+
+
+});
+
+
+
+require __DIR__.'/auth.php';
